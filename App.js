d4e29a09fde89ccbdc3f9cfd94d6774a74e873a6
YusronFauzanN <Yusronsronz7@gmail.com>
2023-11-19 15:24:21 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello World! React Native</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => alert('You Touched Me!')}
+      >
+        <Text style={styles.buttonText}>Touch Me!</Text>
+      </TouchableOpacity>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -17,4 +24,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    marginBottom: 6,
+  },
+  buttonText: {
+    color: 'red',
+    fontSize: 14,
+  },
+  button: {
+    marginTop: 6,
+    paddingTop: 2,
+    paddingLeft: 4,
+    paddingRight: 4,
+    paddingBottom: 2,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'red',
+  }
 });
