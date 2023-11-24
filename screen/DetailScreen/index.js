c8924ab53d890c12ba4e57aa13cc9d2927dab2f0
YusronFauzanN <Yusronsronz7@gmail.com>
2023-11-24 14:59:46 +0700

    feature: add more screen, redux

--- screen/DetailScreen/index.js
+++ screen/DetailScreen/index.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import { useRoute } from '@react-navigation/native';
-import { Button, Text, View } from 'native-base';
+import React from "react";
+import { View, Text, Image, ScrollView } from "react-native";
+import { Button } from "native-base";
+import StarRating from "../../components/StarRating";
 
-function DetailScreen({navigation}) {
-  const route = useRoute();
-  const { param } = route.params;
+const HotelDetailScreen = ({ route }) => {
+  const { hotel } = route.params;
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Detail Screen!</Text>
-      <Text>Param1: {param}</Text>
-      <Button small primary onPress={() => navigation.navigate('Booking', {param: 'Booking Hotel'})}>
-        <Text>Book Hotel (Param Text)</Text>
-      </Button>
-    </View>
+    <ScrollView>
+      <View style={{ alignItems: "center", padding: 16 }}>
+        <Image
+          source={{ uri: hotel.main_photo_url }}
+          style={{ width: 200, height: 200, borderRadius: 8 }}
+        />
+        <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 16 }}>
+          {hotel.hotel_name}
+        </Text>
+        <Text style={{ fontSize: 16, color: "gray", marginTop: 8 }}>
+          {hotel.address}
+        </Text>
+        <StarRating rating={hotel.review_score} />
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 16 }}>
+          Description:
+        </Text>
+        <Text>{/* Add hotel description here */}</Text>
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 16 }}>
+          Facilities:
+        </Text>
+        <Text>{/* Add hotel facilities here */}</Text>
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 16 }}>
+          Price: Rp.{" "}
+          {hotel.price_breakdown.all_inclusive_price.toLocaleString("en-US")}
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 16 }}>
+          Reviews:
+        </Text>
+        <Text>Number of Reviews: {hotel.review_nr}</Text>
+        {/* Add more review details if needed */}
+        <Button small primary style={{ marginTop: 16 }}>
+          <Text>Book Now</Text>
+        </Button>
+      </View>
+    </ScrollView>
   );
-}
+};
 
-export default DetailScreen;
+export default HotelDetailScreen;
