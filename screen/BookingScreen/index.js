e92bb32dae549c304bbd953a896c367fe26d81c7
bimajr <120031668+bimajr@users.noreply.github.com>
2023-12-04 02:20:34 +0800

    add checkin and checkout buttons

--- screen/BookingScreen/index.js
+++ screen/BookingScreen/index.js
@@ -13,27 +13,32 @@ const BookingScreen = ({ navigation }) => {
   );
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  // const roomDetails = {
-  //   days: 5,
-  //   roomType: "Deluxe Room",
-  //   numberOfPeople: 2,
-  //   pricePerNight: 100,
-  // };
-
-  // const totalCost = roomDetails.days * roomDetails.pricePerNight;
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
   const dispatch = useDispatch();
   const toast = useToast();
   const handleBooking = async () => {
+    if (!checkInDate || !checkOutDate) {
+      toast.show({
+        title: "Please select check-in and check-out dates",
+        status: "warning",
+        placement: "top",
+      });
+      return;
+    }
+
     const bookingHotel = {
       hotel_id,
       hotel_name,
       address,
       price,
       name,
+      email,
       phoneNumber,
-      // checkInDate,
-      // checkOutDate,
+      checkInDate,
+      checkOutDate,
     };
+
     dispatch(bookHotel(bookingHotel));
     toast.show({
       title: "Success",
@@ -53,6 +58,61 @@ const BookingScreen = ({ navigation }) => {
           borderRadius: 10,
         }}
       >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            color: "#333",
+          }}
+        >
+          Choose Dates
+        </Text>
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 20,
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ color: "#555", marginBottom: 8 }}>
+            Check-in:{" "}
+            {checkInDate ? checkInDate.toDateString() : "Not selected"}
+          </Text>
+          <Text style={{ color: "#555", marginBottom: 16 }}>
+            Check-out:{" "}
+            {checkOutDate ? checkOutDate.toDateString() : "Not selected"}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              backgroundColor: "white",
+              padding: 0,
+              borderRadius: 10,
+            }}
+          >
+            <Button
+              onPress={() => {}}
+              style={{
+                backgroundColor: "#3498db",
+                borderRadius: 8,
+                marginRight: 8,
+              }}
+            >
+              <Text style={{ color: "white" }}>Select Check-in Date</Text>
+            </Button>
+            <Button
+              onPress={() => {}}
+              style={{
+                backgroundColor: "#3498db",
+                borderRadius: 8,
+              }}
+            >
+              <Text style={{ color: "white" }}>Select Check-out Date</Text>
+            </Button>
+          </View>
+        </View>
         <Text
           style={{
             fontSize: 20,
@@ -85,36 +145,6 @@ const BookingScreen = ({ navigation }) => {
           style={{ backgroundColor: "#eee", borderRadius: 5 }}
         />
       </View>
-
-      {/* <View
-        style={{
-          marginTop: 15,
-          backgroundColor: "white",
-          padding: 20,
-          borderRadius: 10,
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: "bold",
-            marginBottom: 16,
-            color: "#333",
-          }}
-        >
-          Price Summary
-        </Text>
-        <Text style={{ color: "#555" }}>
-          Number of Days: {roomDetails.days}
-        </Text>
-        <Text style={{ color: "#555" }}>
-          Number of People: {roomDetails.numberOfPeople}
-        </Text>
-        <Text style={{ fontWeight: "bold", marginTop: 8, color: "#333" }}>
-          Total Cost: ${totalCost}
-        </Text>
-      </View> */}
-
       <View
         style={{
           marginTop: 15,
